6c336e28fb6bde4f695bb93a5d48a4dca3a0f13e
Vaibbhav <Vaibhavsonaewane@gmail.com>
2022-05-12 14:17:04 +0530

    React commite.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.components";
+
+function App() {
+  return (
+    <>
+      <Homepage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Directory from "../../components/directory/directory.conponent";
+import "./homepage.styles.scss";
+
+const Homepage = () => {
+  return (
+    <>
+      <div className="homepage">
+        <Directory />
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
