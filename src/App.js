4270ebaf9ec03f36c09354bb632a6a504e6ad896
Vaibbhav <Vaibhavsonaewane@gmail.com>
2022-05-13 09:03:59 +0530

    Route Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+
 import Homepage from "./pages/homepage/homepage.components";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+const About = () => {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+};
 
 function App() {
   return (
     <>
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </>
   );
 }
