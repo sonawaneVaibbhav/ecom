1943d9871bf85938562d69d70d590e304691a5a7
Vaibbhav <Vaibhavsonaewane@gmail.com>
2022-05-13 12:35:35 +0530

    Shop page and collection added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage/homepage.components";
-
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
-
-const About = () => {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
-};
+import ShopPage from "./pages/shop/shop.components";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="shop" element={<ShopPage />} />
       </Routes>
     </>
   );
